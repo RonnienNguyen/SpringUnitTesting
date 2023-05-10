4820403558a3a9dce561d6160f08f010468aface
RonienNguyen <ndtphuclqd1306@gmail.com>
2023-05-10 19:59:57 +0700

    Create Unit Test Controller

--- src/main/java/com/java/JavaTestingEx/Controller/GetController.java
+++ src/main/java/com/java/JavaTestingEx/Controller/GetController.java
@@ -4,23 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
-
 @RestController
 public class GetController {
 
-    public String hello(String name){
+    @GetMapping("/getResult")
+    public String getResult(@RequestParam(name = "name", defaultValue = "World") String name){
         return String.format("Hello, %s", name);
-    }
-
-    @GetMapping("/hello1")
-    public String helloController(@RequestParam(name = "name", defaultValue = "World") String name){
-        return String.format("Hello, %s", name);
-    }
-
-    @GetMapping("/request")
-    public String getRequest(@RequestParam Optional<String> id){
-        return "ID: " + id.orElseGet(()-> "not provided");
     }
 }
--- src/test/java/com/java/JavaTestingEx/Controller/GetControllerTest.java
+++ src/test/java/com/java/JavaTestingEx/Controller/GetControllerTest.java
@@ -3,10 +3,7 @@ package com.java.JavaTestingEx.Controller;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -14,21 +11,26 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@WebMvcTest(GetControllerTest.class)
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(GetController.class)
 class GetControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
+
     @Test
-    void helloController() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/hello");
-        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
-        assertEquals("Hello, Hello", mvcResult.getResponse().getContentAsString());
+    void getResult() throws Exception{
+        RequestBuilder request = get("/getResult");
+        MvcResult result = mockMvc.perform(request).andReturn();
+        assertEquals("Hello, World", result.getResponse().getContentAsString());
     }
+
     @Test
-    public void testHelloWithName() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello?name=Dan"))
-                .andExpect(content().string("Hello, Dan"));
+    public void testWithGetResult() throws Exception{
+        mockMvc.perform(get("/getResult")).andExpect(content().string("Hello, World"));
     }
 }
